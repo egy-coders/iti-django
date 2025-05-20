2b1f0ff3872055fcb81b031d49f85d9ba945bf92
FARiDHAMiD <faridmohamedfarid@gmail.com>
2025-05-20 22:24:27 +0300

    day 2

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("products.urls"))
+    path('', include("store.urls"))
 ]
--- products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.shortcuts import HttpResponse
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("about/", views.about, name="about")
-]
